3cac6f21da43bac8fa2d0132e4fd0fbcffb57fdb
emelinew1 <emeline.wulstecke@gmail.com>
2023-12-04 12:40:47 +0100

    Creation of filters

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,48 +1,64 @@
 "use strict";
 
-// -----------------------------Variables globales
-const allgallery = document.querySelector(".gallery");
+// -----------------------------Global variables
+const gallery = document.querySelector(".gallery");
 let categories = null;
 
-// -----------------------------Récupération sur l'API
+// -----------------------------API retrieval
 const getWorks = async () => {
     const [galleryResponse, categoriesResponse] = await Promise.all([
-      fetch("http://localhost:5678/api/works")
-      .then(res => res.json()),
-      fetch("http://localhost:5678/api/categories")
-      .then(res => res.json())
+        fetch("http://localhost:5678/api/works")
+            .then(res => res.json()),
+        fetch("http://localhost:5678/api/categories")
+            .then(res => res.json())
     ]);
 
     const works = galleryResponse;
     categories = categoriesResponse;
 
-    // Ajout des travaux à la galerie
+    // Adding work to the gallery
     createGallery(works);
+    createCategory();
 };
 
 getWorks();
 
-//------------- Fonction pour créer la galerie à partir des travaux récupérés
+//------------- Function for creating the gallery 
 const createGallery = (gallery) => {
-  let portfolio = document.getElementById("portfolio");
+    let portfolio = document.getElementById("portfolio");
 
-  portfolio.innerHTML = '';
+    let newGallery = document.createElement("div");
+    newGallery.classList.add("gallery");
 
-  let newGallery = document.createElement("div");
-  newGallery.classList.add("gallery");
+    gallery.forEach((work) => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
 
-  gallery.forEach((work) => {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
+        img.src = work.imageUrl;
+        figcaption.textContent = work.title;
 
-    img.src = work.imageUrl;
-    figcaption.textContent = work.title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        newGallery.appendChild(figure);
+    });
 
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    newGallery.appendChild(figure);
-  });
-
-  portfolio.appendChild(newGallery);
+    portfolio.appendChild(newGallery);
 };
+
+//-------------  Creates a category and adds it to the portfolio 
+const createCategory = () => {
+    const filter = document.createElement("div");
+    filter.classList.add("filter");
+    portfolio.appendChild(filter);
+
+    filter.innerHTML =
+        `<div class="button selected" id="0">Tous</div>` +
+        categories
+            .map(
+                (category, index) =>
+                    `<div class="button" id="${index + 1}">${category.name}</div>`
+            )
+            .join("");
+  };
+
